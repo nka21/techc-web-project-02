2d0e5bd1a82243ec7bcd5cadc0060be705842732
junya3 <obara.junya0728@gmail.com>
2024-11-07 17:11:01 +0900

    FontFamilyの設定

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        kaisei: ['Kaisei Decol', 'serif'],
+        kurenaido: ['Zen Kurenaido', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
